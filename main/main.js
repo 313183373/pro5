6a45db999a90c924da3d6e770310dc87f2676a45
star xiong <313183373@qq.com>
2017-05-28 20:17:07 +0800

    1

--- main/main.js
+++ main/main.js
@@ -1,6 +1,22 @@
 const loadAllItems = require('./loadAllItems.js');
 
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(inputs) {
+	var numOfItems=[0,0,0,0,0,0];
+	var sum=0;
+	for(let i=0;i<input.length;i++){
+		for(let j=0;j<loadAllItems.length;j++){
+			if(loadAllItems[j][barcode]==input[i]){
+				numOfItems[j]++;
+				sum+=loadAllItems[j][price];
+			}
+		}
+	}
+	var str='***<没钱赚商店>购物清单***';
+	for(let i=0;i<numOfItems.length;i++){
+		if(numOfItems[i]!=0){
+			str+='\n名称：'+loadAllItems[i][name]+',数量：'+numOfItems[i]+loadAllItems[i][unit]+',单价：'+loadAllItems[i][price]+'(元),小计：'+loadAllItems[i][price]*numOfItems[i]+'(元)';
+		}
+	}
+	str+='\n----------------------\n总计：'+sum+'(元)'+'**********************';
+	return str;
 };
